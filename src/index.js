bec4ab7d4667b50eac63012db59f3b828a0c5ab0
Victor <55296594+vicyarmots@users.noreply.github.com>
2019-11-14 23:58:13 +0300

    add price cards

--- src/index.js
+++ src/index.js
@@ -5,3 +5,27 @@ tabs.addEventListener('click', ({ target }) =>
     ? target.classList.remove('is-current-tab')
     : target.classList.add('is-current-tab')
 );
+
+const tabs = document.querySelector('.features-tabs');
+
+tabs.addEventListener('click', ({ target }) =>
+  target.classList.contains('is-current-tab')
+    ? target.classList.remove('is-current-tab')
+    : target.classList.add('is-current-tab')
+);
+
+const tabs = document.querySelector('.features-tabs');
+
+tabs.addEventListener('click', ({ target }) =>
+  target.classList.contains('is-current-tab')
+    ? target.classList.remove('is-current-tab')
+    : target.classList.add('is-current-tab')
+);
+
+const tabs = document.querySelector('.features-tabs');
+
+tabs.addEventListener('click', ({ target }) =>
+  target.classList.contains('is-current-tab')
+    ? target.classList.remove('is-current-tab')
+    : target.classList.add('is-current-tab')
+);
